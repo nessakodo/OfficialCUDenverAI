1bb5f95f18a769bbef540704964becac2a3e6142
unknown <elyas.larfi@ucdenver.edu>
2025-01-21 21:33:13 -0700

    Fixed store path issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './stores/Store';
+import store from './stores/store';
 import App from './App'; 
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
